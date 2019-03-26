0be2f6ef8683dd2a08d3dbb3c61e0fe556ff4f3d
Frank Martinez <frankie.martinez@gmail.com>
2019-03-26 15:00:09 -0700

    Put, delete done with minimal error messaging

--- zoos/router.js
+++ zoos/router.js
@@ -68,7 +68,7 @@ router.put('/:id', (req, res) => {
                 res.status(200).json(count);
             } else {
                 res.status(404).json({ 
-                    message: 'Record not found' 
+                    message: "Record not found"
                 });
             }
         })
@@ -77,6 +77,23 @@ router.put('/:id', (req, res) => {
         });
 });
 
+
+
+router.delete('/:id', (req, res) => {
+    zooDB('zoos')
+      .where({ id: req.params.id })
+      .del()
+      .then(count => {
+        if (count > 0) {
+          res.status(204).end();
+        } else {
+          res.status(404).json({ message: 'Record not found' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
 
 module.exports = router;
 
